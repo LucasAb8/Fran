06e66d1ac11cb9787404de6daaf5dc8bc480070d
unknown <lucasabruzzo8@gmail.com>
2022-07-19 11:58:14 -0300

    first-build

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,10 @@
+import { isPreviewMode } from '@decentraland/EnvironmentAPI'
+import { triggerEmote, PredefinedEmote } from "@decentraland/RestrictedActions"
+
+
+
 const building = new Entity()
-building.addComponent(new GLTFShape("models/bil.glb"))
+building.addComponent(new GLTFShape("models/fuego.glb"))
 building.addComponent(
   new Transform({
     position: new Vector3(8, 0, 8),
@@ -9,194 +14,94 @@ building.addComponent(
 
 engine.addEntity(building)
 
-const eye = new Entity()
-eye.addComponent(new GLTFShape("models/ojo.glb"))
-eye.addComponent(
+
+
+
+
+const shoes = new Entity()
+shoes.addComponent(new GLTFShape("models/shoes.glb"))
+shoes.addComponent(
+  new Transform({
+    position: new Vector3(8, 0, 8),
+    scale: new Vector3(1, 1, 1),
+  })
+)
+shoes.addComponent(
+  new OnPointerDown(() => {
+    openExternalURL("https://market.decentraland.org/contracts/0x7cd58cb897f97019354e23d19f46d15407ce870d/items/0")
+  })
+)
+
+engine.addEntity(shoes)
+
+
+
+const ig = new Entity()
+ig.addComponent(new GLTFShape("models/ig.glb"))
+ig.addComponent(
+  new Transform({
+    position: new Vector3(8, 0, 8),
+    scale: new Vector3(1, 1, 1),
+  })
+)
+ig.addComponent(
+  new OnPointerDown(() => {
+    openExternalURL("https://www.instagram.com/fuego.dance/")
+  })
+)
+
+engine.addEntity(ig)
+
+
+
+
+const tw = new Entity()
+tw.addComponent(new GLTFShape("models/tw.glb"))
+tw.addComponent(
+  new Transform({
+    position: new Vector3(8, 0, 8),
+    scale: new Vector3(1, 1, 1),
+  })
+)
+tw.addComponent(
+  new OnPointerDown(() => {
+    openExternalURL("https://twitter.com/Fuego_dance")
+  })
+)
+
+engine.addEntity(tw)
+
+
+
+
+const lights = new Entity()
+lights.addComponent(new GLTFShape("models/lights.glb"))
+lights.addComponent(
   new Transform({
     position: new Vector3(8, 0, 8),
     scale: new Vector3(1, 1, 1),
   })
 )
 
-engine.addEntity(eye)
+engine.addEntity(lights)
 
-const eye2 = new Entity()
-eye2.addComponent(new GLTFShape("models/ojo.glb"))
-eye2.addComponent(
+
+const fdance = new Entity()
+fdance.addComponent(new GLTFShape("models/fuegodance.glb"))
+fdance.addComponent(
   new Transform({
-    position: new Vector3(8, -0.5, 8),
-    scale: new Vector3(0.9, 1, 0.9),
+    position: new Vector3(8, 0, 8),
+    scale: new Vector3(1, 1, 1),
   })
 )
 
-engine.addEntity(eye2)
-
+engine.addEntity(fdance)
 
 
 
 //NFTs=================================================================================================
 
 
-const c1 = new Entity()
-c1.addComponent(new GLTFShape("models/c1.glb"))
-c1.addComponent(
-  new Transform({
-    position: new Vector3(10.5, 0,8 ),
-    scale: new Vector3(1, 1, 1),
-  })
-)
-c1.addComponent(
-  new OnPointerDown((e) => {
-    openNFTDialog(
-      "ethereum://0x85d39cea74b0baba54d7fd0df42dd3e6e39b1625/3615"
-    )
-  })
-)
-engine.addEntity(c1)
-
-
-
-const c2 = new Entity()
-c2.addComponent(new GLTFShape("models/c2.glb"))
-c2.addComponent(
-  new Transform({
-    position: new Vector3(10.5, 0, 8),
-    scale: new Vector3(1, 1, 1),
-  })
-)
-c2.addComponent(
-  new OnPointerDown((e) => {
-    openNFTDialog(
-      "ethereum://0x85d39cea74b0baba54d7fd0df42dd3e6e39b1625/3615"
-    )
-  })
-)
-engine.addEntity(c2)
-
-
-
-
-const c3 = new Entity()
-c3.addComponent(new GLTFShape("models/c3.glb"))
-c3.addComponent(
-  new Transform({
-    position: new Vector3(10.5, 0, 8),
-    scale: new Vector3(1, 1, 1),
-  })
-)
-c3.addComponent(
-  new OnPointerDown((e) => {
-    openNFTDialog(
-      "ethereum://0x85d39cea74b0baba54d7fd0df42dd3e6e39b1625/3615"
-    )
-  })
-)
-engine.addEntity(c3)
-
-
-
-const c4 = new Entity()
-c4.addComponent(new GLTFShape("models/c4.glb"))
-c4.addComponent(
-  new Transform({
-    position: new Vector3(10.5, 0, 8),
-    scale: new Vector3(1, 1, 1),
-  })
-)
-c4.addComponent(
-  new OnPointerDown((e) => {
-    openNFTDialog(
-      "ethereum://0x85d39cea74b0baba54d7fd0df42dd3e6e39b1625/3615"
-    )
-  })
-)
-engine.addEntity(c4)
-
-
-
-
-
-
-
-
-const c5 = new Entity()
-c5.addComponent(new GLTFShape("models/c5.glb"))
-c5.addComponent(
-  new Transform({
-    position: new Vector3(6, 0, 8),
-    scale: new Vector3(1, 1, 1),
-  })
-)
-c5.addComponent(
-  new OnPointerDown((e) => {
-    openNFTDialog(
-      "ethereum://0x85d39cea74b0baba54d7fd0df42dd3e6e39b1625/3615"
-    )
-  })
-)
-engine.addEntity(c5)
-
-
-
-const c6 = new Entity()
-c6.addComponent(new GLTFShape("models/c6.glb"))
-c6.addComponent(
-  new Transform({
-    position: new Vector3(6, 0, 8),
-    scale: new Vector3(1, 1, 1),
-  })
-)
-c6.addComponent(
-  new OnPointerDown((e) => {
-    openNFTDialog(
-      "ethereum://0x85d39cea74b0baba54d7fd0df42dd3e6e39b1625/3615"
-    )
-  })
-)
-engine.addEntity(c6)
-
-
-
-
-const c7 = new Entity()
-c7.addComponent(new GLTFShape("models/c7.glb"))
-c7.addComponent(
-  new Transform({
-    position: new Vector3(6, 0, 8),
-    scale: new Vector3(1, 1, 1),
-  })
-)
-c7.addComponent(
-  new OnPointerDown((e) => {
-    openNFTDialog(
-      "ethereum://0x85d39cea74b0baba54d7fd0df42dd3e6e39b1625/3615"
-    )
-  })
-)
-engine.addEntity(c7)
-
-
-
-
-
-
-
-const c8 = new Entity()
-c8.addComponent(new GLTFShape("models/c8.glb"))
-c8.addComponent(
-  new Transform({
-    position: new Vector3(6, 0, 8),
-    scale: new Vector3(1, 1, 1),
-  })
-)
-c8.addComponent(
-  new OnPointerDown((e) => {
-    openNFTDialog(
-      "ethereum://0x85d39cea74b0baba54d7fd0df42dd3e6e39b1625/3615"
-    )
-  })
-)
-engine.addEntity(c8)
 
 
 
@@ -204,40 +109,79 @@ engine.addEntity(c8)
 
 //NFTs=================================================================================================
 
-const canvas = new UICanvas()
-const text = new UIText(canvas)
-const text2 = new UIText(canvas)
-const text3 = new UIText(canvas)
-
-
-text.positionY=0
-text3.positionY=20
-text2.positionY=40
-
-text.value=<string><unknown>Camera.instance.position.x
-text2.value=<string><unknown>Camera.instance.position.y 
-text3.value=<string><unknown>Camera.instance.position.z 
+var n=300;
+var t = 300;
+var jump =0;
 
 
 
-export class RotatorSystem {
+
+
+class AutoDance {
   // The update() function runs on every frame.
   update() {
-    text.value=<string><unknown>Camera.instance.position.x
-    text2.value=<string><unknown>Camera.instance.position.y 
-    text3.value=<string><unknown>Camera.instance.position.z 
-    }
+    
+    Input.instance.subscribe('BUTTON_DOWN', ActionButton.JUMP, false, () => {
+      n = t-20;
+      jump= 1;})
+    Input.instance.subscribe('BUTTON_UP', ActionButton.FORWARD, false, () => {
+      if (jump == 1){
+        n = t - 10;
+      }
+      else{
+        n = t;
+      }})
+    Input.instance.subscribe('BUTTON_UP', ActionButton.LEFT, false, () => {
+      if (jump == 1){
+        n = t - 10;
+      }
+      else{
+        n = t;
+      }})
+    Input.instance.subscribe('BUTTON_UP', ActionButton.RIGHT, false, () => {
+      if (jump == 1){
+        n = t - 10;
+      }
+      else{
+        n = t;
+      }})
+    Input.instance.subscribe('BUTTON_UP', ActionButton.BACKWARD, false, () => {
+      if (jump == 1){
+        n = t - 10;
+      }
+      else{
+        n = t;
+      }})
+    if ( n%t ==0 &&
+      Camera.instance.position.x <= 15.2 && Camera.instance.position.x >= 1.8
+      && Camera.instance.position.z <= 15 && Camera.instance.position.z >=1.5 
+      && 2 < Math.sqrt(Math.pow(Camera.instance.position.x - 8.4,2)+
+      Math.pow(Camera.instance.position.z - 14.8,2))){
+        if(jump == 1){
+          jump = 0;
+        }
+        if(Camera.instance.position.y > 10 && Camera.instance.position.y <10.6){
+          triggerEmote({ predefined: PredefinedEmote.DISCO })
+          t= 300;
+          
+        }
+        else if(Camera.instance.position.y > 10.8){
+          triggerEmote({ predefined: PredefinedEmote.TEKTONIK})
+          t = 305;
+        }
+        n =0;
+      }
+      n+=1;
+
   }
-  engine.addSystem(new RotatorSystem())
-  
+}
+engine.addSystem(new AutoDance())
 
 
-  
-  
   //VIDEO==================================================================================
-  
+  /*
   const myVideoClip = new VideoClip(
-    "https://ipfs.io/ipfs/bafybeifffz6ejfpkfddfc2h6k23xflntanw4vhdjcyoldu5xsdrsntdzbq/video.mp4")
+    //"https://ipfs.io/ipfs/bafybeifffz6ejfpkfddfc2h6k23xflntanw4vhdjcyoldu5xsdrsntdzbq/video.mp4")
   
   
   const myVideoTexture = new VideoTexture(myVideoClip)
@@ -284,4 +228,4 @@ building.addComponent(source)
 
 // Play sound
 source.playing = true
-source.loop = true
+source.loop = true*/
